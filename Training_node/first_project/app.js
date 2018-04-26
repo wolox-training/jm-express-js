a4ffd569ddfb883b132e8cc78997d7bc3e0a5632
Julian Molina <julian.molina@wolox.com.ar>
2018-04-26 09:32:17 -0300

    prueba TravisCI

--- Training_node/first_project/app.js
+++ Training_node/first_project/app.js
@@ -25,7 +25,7 @@ const bodyParserUrlencodedConfig = () => ({
 const init = () => {
   const app = express();
   const port = config.common.port || 8080;
-  module.exports = app;
+  module.exports =  app;
 
   app.use('/docs', express.static(path.join(__dirname, 'docs')));
 
