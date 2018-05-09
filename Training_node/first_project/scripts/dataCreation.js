e24cfc1f39fa0ef443544a1fd9eb830640b779cd
Julian Molina <julian.molina@wolox.com.ar>
2018-05-09 11:42:28 -0300

    remove unused code

--- Training_node/first_project/scripts/dataCreation.js
+++ Training_node/first_project/scripts/dataCreation.js
@@ -1,27 +1,3 @@
-// exports.execute = () => {
-//   // This function should create data for testing and return a promise
-// };
-
-const bcrypt = require('bcryptjs'),
-  User = require('../app/models').user;
-
 exports.execute = () => {
-  return bcrypt
-    .hash('hola12345', 10)
-    .then(hash => {
-      const data = [];
-      data.push(
-        User.create({
-          firstName: 'firstName1',
-          lastName: 'lastName1',
-          email: 'email1@wolox.com.ar',
-          password: hash
-        })
-      );
-
-      return Promise.all(data);
-    })
-    .catch(bcryptErr => {
-      throw bcryptErr;
-    });
+  // This function should create data for testing and return a promise
 };
